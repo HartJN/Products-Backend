e95c294dca704dd919f5f1d64fb2dbb9960cadcd
HartJN <jameshart04@gmail.com>
2023-02-19 11:06:44 +1300

    setup jest

---
+++ config/test.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  port: process.env.PORT,
+  dbUri: process.env.DB_URI,
+  saltWorkFactor: process.env.SALT_ROUNDS,
+  accessTokenTtl: process.env.ACCESS_TOKEN_TTL,
+  refreshTokenTtl: process.env.REFRESH_TOKEN_TTL,
+  publicKey: process.env.PUBLIC_KEY,
+  privateKey: process.env.PRIVATE_KEY,
+};
