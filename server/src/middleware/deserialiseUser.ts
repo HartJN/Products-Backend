39691882627ce468107c5ebd4c257210085096a8
HartJN <jameshart04@gmail.com>
2023-02-19 14:40:14 +1300

    add ability to choose cookie or localstorage

--- server/src/middleware/deserialiseUser.ts
+++ server/src/middleware/deserialiseUser.ts
@@ -3,10 +3,17 @@ import { get } from 'lodash';
 import { reIssueAccessToken } from '../service/session.service';
 import { verifyJwt } from '../utils/jwt.utils';
 
-const deserializeUser = async (req: Request, res: Response, next: NextFunction) => {
-  const accessToken = get(req, 'headers.authorization', '').replace(/^Bearer\s/, '');
+const deserializeUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const accessToken =
+    get(req, 'cookies.accessToken') ||
+    get(req, 'headers.authorization', '').replace(/^Bearer\s/, '');
 
-  const refreshToken = get(req, 'headers.x-refresh');
+  const refreshToken =
+    get(req, 'cookies.refreshToken') || get(req, 'headers.x-refresh');
 
   if (!accessToken) {
     return next();
@@ -21,11 +28,22 @@ const deserializeUser = async (req: Request, res: Response, next: NextFunction) 
 
   if (expired && refreshToken) {
     const newAccessToken = await reIssueAccessToken({
-      refreshToken: Array.isArray(refreshToken) ? refreshToken[0] : refreshToken,
+      refreshToken: Array.isArray(refreshToken)
+        ? refreshToken[0]
+        : refreshToken,
     });
 
     if (newAccessToken) {
       res.setHeader('x-access-token', newAccessToken);
+
+      res.cookie('accessToken', newAccessToken, {
+        maxAge: 900000,
+        httpOnly: true,
+        domain: 'localhost',
+        path: '/',
+        sameSite: 'strict',
+        secure: process.env.NODE_ENV === 'production' ? true : false,
+      });
     }
 
     const result = verifyJwt(newAccessToken);
