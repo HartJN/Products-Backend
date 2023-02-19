30014b50fd7cfcfcf2fff952decee7ef59c892f1
HartJN <jameshart04@gmail.com>
2023-02-19 14:36:36 +1300

    add function to get current user

--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import logger from '../utils/logger';
 import { createUser } from '../service/user.service';
 import { CreateUserInput } from '../schema/user.schema';
+import config from 'config';
 
 export async function createUserHandler(
   req: Request<{}, {}, CreateUserInput['body']>,
@@ -9,9 +10,14 @@ export async function createUserHandler(
 ) {
   try {
     const user = await createUser(req.body);
+
     return res.send(user);
   } catch (err: any) {
     logger.error(err);
     return res.status(409).send(err.message);
   }
+}
+
+export async function getCurrentUser(req: Request, res: Response) {
+  return res.send(res.locals.user);
 }
