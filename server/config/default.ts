aa511ca29c51cb94a8d50f01ec127f1a777a6cfc
HartJN <jameshart04@gmail.com>
2023-02-19 14:42:08 +1300

    add origin to config

--- server/config/default.ts
+++ server/config/default.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 export default {
   port: process.env.PORT,
+  origin: process.env.ORIGIN_URL,
   dbUri: process.env.DB_URI,
   saltWorkFactor: process.env.SALT_ROUNDS,
   accessTokenTtl: process.env.ACCESS_TOKEN_TTL,
