0ed9d47eccf4b143d96b3b07a380f25be09dfef6
HartJN <jameshart04@gmail.com>
2023-02-19 14:13:58 +1300

    fix server config

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,10 @@
 import config from 'config';
-
 import connect from './utils/connect';
 import logger from './utils/logger';
 import createServer from './utils/server';
 
-const port = config.get<number>('port');
-
 const app = createServer();
+const port = config.get<number>('port');
 
 app.listen(port, async () => {
   logger.info(`Server is running on port ${port}`);
--- server/src/utils/server.ts
+++ server/src/utils/server.ts
@@ -1,12 +1,25 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import deserialiseUser from '../middleware/deserialiseUser';
 import routes from '../routes';
+import setHeaders from '../middleware/setHeaders';
+import cors from 'cors';
+import config from 'config';
 
 function createServer() {
   const app = express();
 
-  app.use(express.json());
+  app.use(
+    cors({
+      origin: config.get('origin'),
+      credentials: true,
+    })
+  );
 
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use(cookieParser());
   app.use(deserialiseUser);
 
   routes(app);
