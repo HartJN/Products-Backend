dde2daf5c93a7f7103e3088f8f29ebfe7500489a
HartJN <jameshart04@gmail.com>
2023-02-21 11:32:03 +1300

    update config

--- server/config/custom-environment-variables.ts
+++ server/config/custom-environment-variables.ts
@@ -3,4 +3,11 @@ export default {
   privatekey: 'PRIVATE_KEY',
   dbUri: 'PRODUCTION_DB_CONNECTION',
   port: 'PORT',
+  origin: 'ORIGIN_URL',
+  saltWorkFactor: 'SALT_ROUNDS',
+  accessTokenTtl: 'ACCESS_TOKEN_TTL',
+  refreshTokenTtl: 'REFRESH_TOKEN_TTL',
+  googleClientId: 'AUTHO_CLIENT_ID',
+  googleClientSecret: 'AUTHO_SECRET',
+  googleOAuthRedirectUri: 'AUTHO_REDIRECT_URL',
 };
